900054a3b7e5e12c08b72c2c86611b1d2068e98a
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-05-31 10:20:23 -0700

    Initial Commit

---
+++ GoVenture/GoVentureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoVentureApp.swift
+//  GoVenture
+//
+//  Created by Daniel Paymar on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct GoVentureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MapView()
+        }
+    }
+}
---
+++ GoVenture/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  GoVenture
+//
+//  Created by Daniel Paymar on 5/31/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    MapView()
+}
